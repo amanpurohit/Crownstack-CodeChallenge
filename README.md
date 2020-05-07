22a81b4c95ec441ae25a7d9d25a6472545232f84
Aman Purohit <amanpurohit12@gmail.com>
2020-05-07 15:34:37 +0530

    (release) Version 1

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ The code challenge is the simple project where we are building a part of sample 
 Assume a company which rents heavy equipments to its customer. We are showing the catalog for that system in this code challenge.
 
 **Technology Stack**
+- Angular CLI: 9.1.4
+- Node: 13.13.0
+- Angular: 9.1.4
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ajax, AjaxResponse } from 'rxjs/ajax';
+import { map, catchError } from 'rxjs/operators'
+import { of } from 'rxjs';
+import { LocationData, Branch, Category } from './module';
+
+const DATA_SOURCE = 'assets/catalog.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'crownstack';
+export class AppComponent implements OnInit {
+
+  locationData: LocationData[];
+  selectedLocation: Array<Category>;
+  selectedCategory: Category;
+  constructor() {}
+
+  ngOnInit() {
+    // Using rxjs/ajax instead of Angular HttpClient Module
+    const catalogData$ = ajax(DATA_SOURCE)
+    catalogData$.pipe(
+      map((data: AjaxResponse) => {return data.response}),
+      map((apiResponse: any) => { return apiResponse.data.locations }),
+      catchError(err => of([]))
+    )
+    .subscribe((_locationData: LocationData[]) => {
+      this.locationData = _locationData
+    });
+
+  }
+
+  selectCategory(category) {
+    this.selectedCategory = category;
+  }
+
 }
