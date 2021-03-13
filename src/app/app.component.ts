c3e0e72a82f08dc571da419229d94e12a8819583
Aman Purohit <amanpurohit12@gmail.com>
2021-03-13 12:33:06 +0530

    refactor(App) - Implemented async pipe for showing location list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ajax, AjaxResponse } from 'rxjs/ajax';
 import { map, catchError } from 'rxjs/operators'
-import { of } from 'rxjs';
-import { LocationData, Branch, Category } from './module';
+import { Observable, of } from 'rxjs';
+import { LocationData, Category } from './module';
 
 const DATA_SOURCE = 'assets/catalog.json';
 
@@ -13,23 +13,18 @@ const DATA_SOURCE = 'assets/catalog.json';
 })
 export class AppComponent implements OnInit {
 
-  locationData: LocationData[];
+  locationData: Observable<LocationData[]>;
   selectedLocation: Array<Category>;
   selectedCategory: Category;
   constructor() {}
 
   ngOnInit() {
     // Using rxjs/ajax instead of Angular HttpClient Module
-    const catalogData$ = ajax(DATA_SOURCE)
-    catalogData$.pipe(
-      map((data: AjaxResponse) => {return data.response}),
-      map((apiResponse: any) => { return apiResponse.data.locations }),
-      catchError(err => of([]))
-    )
-    .subscribe((_locationData: LocationData[]) => {
-      this.locationData = _locationData
-    });
-
+    const catalogData$ = ajax(DATA_SOURCE);
+    this.locationData =  catalogData$.pipe(
+      map((data: AjaxResponse) => data.response.data.locations),
+      catchError(err => of([err]))
+    );
   }
 
   selectCategory(category) {
